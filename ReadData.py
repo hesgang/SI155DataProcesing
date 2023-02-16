0f556a2fbec232afa528008943f2c3f1254aa44a
hesgang <1034680772@qq.com>
2023-02-16 22:28:37 +0800

    更新数据读取方法，提升兼容性

--- ReadData.py
+++ ReadData.py
@@ -19,7 +19,7 @@ from logger import logger
 
 
 class ReadData(object):
-    def __init__(self, excel_path, sheet_name=0):
+    def __init__(self, excel_path):
         self.tmp = self.__getTemp__()
         self.excel_path = excel_path
         self.path, self.file = os.path.split(self.excel_path)
@@ -28,7 +28,7 @@ class ReadData(object):
         self.file_type = self.file.split('.')[1]
         self.npy_path = os.path.join(self.tmp, self.b64_name + '.npy')
         self.npy_c_path = os.path.join(self.tmp, self.b64_name + '_c' + '.npy')
-        self.sheet_name = sheet_name
+        # self.sheet_name = sheet_name
 
     def __read_data(self):
         '''
@@ -42,22 +42,35 @@ class ReadData(object):
                 excel_t = os.path.getmtime(self.excel_path)
                 npy_t = os.path.getmtime(self.npy_path)
                 if npy_t > excel_t:
-                    df = np.load(self.npy_path, allow_pickle=True)
+                    _df = np.load(self.npy_path, allow_pickle=True)
                     df_c = np.load(self.npy_c_path, allow_pickle=True)
-                    df = pd.DataFrame(df, columns=df_c)
-                    return df
+                    _df = pd.DataFrame(_df, columns=df_c)
+                    return _df
                 else:
                     raise FileNotFoundError
             except FileNotFoundError:
-                df = pd.read_excel(self.excel_path, header=0, sheet_name=self.sheet_name)
-                df_c = np.array(df.columns.values)
-                data_ndarray = df.to_numpy()
-                np.save(self.npy_path, data_ndarray)
-                np.save(self.npy_c_path, df_c)
-                return df
+                _df = pd.ExcelFile(self.excel_path)
+                if len(_df.sheet_names) == 1:
+                    df_1 = pd.read_excel(_df, header=0)
+                    col_types = list(set(df_1.dtypes.astype(str).to_list()))
+                    for _i in col_types:
+                        if _i not in ['float64', 'int64']:
+                            logger.debug('存在无法转换为ndarray的数据类型：{}.直接读取，不做缓存！'.format(_i))
+                            return df_1
+                    self.__cache__(df_1)
+                    return df_1
+                else:
+                    logger.info('该数据簿存在多个sheet，如下：\n {}'.format(_df.sheet_names))
+                    return _df
             except Exception as e:
                 logger.error(e)
                 sys.exit()
+
+    def __cache__(self, df):
+        df_c = np.array(df.columns.values)
+        data_ndarray = df.to_numpy()
+        np.save(self.npy_path, data_ndarray)
+        np.save(self.npy_c_path, df_c)
 
     @staticmethod
     def __getTemp__() -> str:
@@ -82,8 +95,13 @@ class ReadData(object):
 
 
 if __name__ == '__main__':
-    start_time = time.process_time()
-    aa = ReadData(r'D:\code\DataProcessing\data\2022\s5-20220805151008.xlsx').get_df
-    end = time.process_time() - start_time
+    start_time = time.time()
+    # f = pd.ExcelFile(r'D:\Temp\滑动4N-全过程.xlsx')
+    aa = ReadData(r'D:\Temp\滑动4N-全过程.xlsx').get_df
+    print(aa.head())
+    end = time.time() - start_time
     print('总耗时：%s' % end)
 
+    #     df1 = f.parse(sheet_name=i)
+    #     df1.drop('Timestamp', axis=1, inplace=True)
+    #     df1.to_excel(writer, sheet_name=i)
---
+++ dataProcessing/流量组专用.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author : hesgang
+# @Desc : 处理流量数据
+import pandas as pd
+import time
+
+
+def drop_1(_df):
+    """
+    基于特定的Excel表格，剔除不满足下列条件的行
+    遍历整个表格
+    :param _df: pd.DataFrame
+    :return: pd.DataFrame
+    """
+    # 判断数据类型是否正确
+    if not isinstance(_df, pd.DataFrame):
+        raise TypeError('Unexpected {}, <pd.Series or pd.DataFrame> is need!'.format(type(_df)))
+    for item in _df.index:
+        # if语句为判断的条件及列名位置
+        if _df['# CH 1'][item] != 1 or _df['# CH 2'][item] != 1:
+            _df.drop(item, inplace=True)
+    return _df
+
+
+def drop_3sigma(
+        _df: pd.DataFrame,
+        col_name: 'str | list[str]'
+) -> pd.DataFrame:
+    """
+    基于特定的Excel表格，对表格某一列或者两列数据进行3sigma处理，并删除3sigma之外的数据
+    :param _df: 需要处理的表格
+    :param col_name: 列名
+    :return: 处理之后的表格
+    """
+    # 判断数据类型是否正确
+    if not isinstance(_df, pd.DataFrame):
+        raise TypeError('Unexpected {}, <pd.Series or pd.DataFrame> is need!'.format(type(_df)))
+
+    if type(col_name) == str:
+        col_mean = round(_df[col_name].mean(), 10)  # 平均值
+        col_std = round(_df[col_name].std(), 10)  # 标准差
+        up3sigma = round(col_mean + 3 * col_std, 10)
+        down3sigma = round(col_mean - 3 * col_std, 10)
+        _df = _df[(_df[col_name] <= up3sigma) | (_df[col_name] >= down3sigma)]
+    else:
+        for item in col_name:
+            col_mean = round(_df[item].mean(), 10)  # 平均值
+            col_std = round(_df[item].std(), 10)  # 标准差
+            up3sigma = round(col_mean + 3 * col_std, 10)  # 较大3sigma处
+            down3sigma = round(col_mean - 3 * col_std, 10)  # 较小3sigma处
+            _df = _df[(_df[item] <= up3sigma) | (_df[item] >= down3sigma)]
+    return _df
+
+
+def save_to_excel(_df, ex_writer, sheet_name):
+    """
+    保存文件，根据需求自己改
+    :param _df:
+    :param ex_writer:
+    :param sheet_name:
+    :return:
+    """
+    # a1 = round(_df['#-0'].mean(), 10)
+    # b1 = round(_df['#-0'].std(), 10)
+    # c1 = round(a1 + 3 * b1, 10)
+    # d1 = round(a1 - 3 * b1, 10)
+    # a2 = round(_df['#-1'].mean(), 10)
+    # b2 = round(_df['#-1'].std(), 10)
+    # c2 = round(a2 + 3 * b2, 10)
+    # d2 = round(a2 - 3 * b2, 10)
+    # _df.at[0, '#-00'] = a1
+    # _df.at[1, '#-00'] = b1
+    # _df.at[2, '#-00'] = c1
+    # _df.at[3, '#-00'] = d1
+    # _df.at[0, '#-01'] = a2
+    # _df.at[1, '#-01'] = b2
+    # _df.at[2, '#-01'] = c2
+    # _df.at[3, '#-01'] = d2
+    _df.to_excel(ex_writer, sheet_name=sheet_name, index=False)
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    f = pd.ExcelFile(r'D:\Temp\merge.xlsx')
+    writer = pd.ExcelWriter(r'D:\Temp\merge_ok.xlsx')
+    for i in f.sheet_names:
+        start_time1 = time.time()
+        dff = f.parse(i)
+        dff = drop_1(dff)
+        for ii in range(2):
+            dff = drop_3sigma(dff, ['#-0', '#-1'])
+        save_to_excel(dff, writer, i)
+        print('处理文件：{}-耗时：{}'.format(i, time.time() - start_time1))
+    end = time.time() - start_time
+    print('处理总耗时：%s' % end)
+    writer.save()
+    end = time.time() - start_time
+    print('总耗时：%s' % end)
